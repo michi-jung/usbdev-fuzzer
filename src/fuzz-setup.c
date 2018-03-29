edafdebf9dbfdb41b4f9a434ae5d0efe4b362d17
Michael Jung <mijung@gmx.net>
2018-03-29 10:01:53 +0200

    First transfer on BULK IN

--- src/fuzz-setup.c
+++ src/fuzz-setup.c
@@ -4,7 +4,8 @@
 int usbdev_fuzz_setup(libusb_device_handle *dev)
 {
 	char hello[] = "Hello NuttX!";
-	int rc = 0;
+	char buffer[256];
+	int rc = 0, transferred = 0;
 
 	rc = libusb_set_configuration(dev, 1);
 	if (rc) {
@@ -23,11 +24,22 @@ int usbdev_fuzz_setup(libusb_device_handle *dev)
 	rc = libusb_control_transfer(dev, LIBUSB_ENDPOINT_OUT | LIBUSB_REQUEST_TYPE_VENDOR |
 				     LIBUSB_RECIPIENT_DEVICE, 0x01, 0x100, 0,
 				     (unsigned char *)hello, sizeof(hello), 100);
-	if (rc) {
+	if (rc < 0) {
 		fprintf(stderr, "libusb_control_transfer() failed. rc: %d\n",
 			rc);
 		goto done;
 	}
+
+	rc = libusb_bulk_transfer(dev, LIBUSB_ENDPOINT_IN | 0x02,
+				  (unsigned char *)buffer, sizeof(buffer),
+				  &transferred, 100);
+	if (rc) {
+		fprintf(stderr, "libusb_bulk_transfer() failed. rc: %d\n",
+			rc);
+		goto done;
+	}
+
+        printf("Received (%d): '%.*s'\n", transferred, transferred, buffer);
 
 done:
 	libusb_release_interface(dev, 0);
